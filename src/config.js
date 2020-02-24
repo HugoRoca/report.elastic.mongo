4fba32e2d1dffc1fd3a0e1b5514867644d131344
Hugo Roca <hugo.rock20@hotmail.com>
2020-02-24 10:12:19 -0500

    replace

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
     MONGODB: {
         CLUSTERS: [
             {
-                ENDPOINT: "mongodb://usrmongotdapp:Mongo2018@PersonalizacionPRD01-shard-00-00-7arob.mongodb.net:27017,PersonalizacionPRD01-shard-00-01-7arob.mongodb.net:27017,PersonalizacionPRD01-shard-00-02-7arob.mongodb.net:27017/admin?ssl=true&replicaSet=PersonalizacionPRD01-shard-0&authSource=admin",
+                ENDPOINT: "...",
                 COUNTRIES: {
                     CO: "BelcorpColombia",
                     BO: "BelcorpBolivia",
@@ -11,7 +11,7 @@ const config = {
                 }
             },
             {
-                ENDPOINT: "mongodb://usrmongotdapp:Mongo2018@PersonalizacionPRD02-shard-00-00-7arob.mongodb.net:27017,PersonalizacionPRD02-shard-00-01-7arob.mongodb.net:27017,PersonalizacionPRD02-shard-00-02-7arob.mongodb.net:27017/admin?ssl=true&replicaSet=PersonalizacionPRD02-shard-0&authSource=admin",
+                ENDPOINT: "...",
                 COUNTRIES: {
                     PE: "BelcorpPeru",
                     CL: "BelcorpChile",
@@ -20,7 +20,7 @@ const config = {
                 }
             },
             {
-                ENDPOINT: "mongodb://usrmongotdapp:Mongo2018@PersonalizacionPRD03-shard-00-00-7arob.mongodb.net:27017,PersonalizacionPRD03-shard-00-01-7arob.mongodb.net:27017,PersonalizacionPRD03-shard-00-02-7arob.mongodb.net:27017/admin?ssl=true&replicaSet=PersonalizacionPRD03-shard-0&authSource=admin",
+                ENDPOINT: "...",
                 COUNTRIES: {
                     MX: "BelcorpMexico",
                     EC: "BelcorpEcuador",
@@ -36,15 +36,15 @@ const config = {
         INDEX_TYPE: "_doc",
         CLUSTERS: [
             {
-                ENDPOINT: "https://vpc-es-sbsearch-prd-a5xq7pyb6cvphjra33ojtejvwa.us-east-1.es.amazonaws.com",
+                ENDPOINT: "https//...",
                 COUNTRIES: ["PE", "CL", "CR", "GT"]
             },
             {
-                ENDPOINT: "https://vpc-es-sbsearch2-prd-zy7ytdwgfleiwpive3meis5lzy.us-east-1.es.amazonaws.com",
+                ENDPOINT: "https//...",
                 COUNTRIES: ["CO", "PA", "DO", "SV"]
             },
             {
-                ENDPOINT: "https://vpc-es-sbsearch3-prd-x6yhgte2h3opuz5lyog56xwtla.us-east-1.es.amazonaws.com",
+                ENDPOINT: "https//...",
                 COUNTRIES: ["MX", "EC", "BO", "PR"]
             }
         ]
--- src/scripts/elasticsearchQuerys.js
+++ src/scripts/elasticsearchQuerys.js
@@ -2,6 +2,11 @@ module.exports = class ElasticsearchQuerys {
     static search() {
         return {
             "size": 0,
+            "query":{
+                "term":{
+                    "activo": true
+                }
+            },
             "aggs": {
                 "personalizaciones": {
                     "terms": {
